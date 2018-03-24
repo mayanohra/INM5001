4e26a03049437e17f5d7f4f11e1bf29dbccdb1e3
Maya N <mayanohra@gmail.com>
2018-03-24 16:15:32 -0400

    application.js: Add global var. error

--- application.js
+++ application.js
@@ -1,21 +1,31 @@
-const formEl = document.getElementById("formulaire");
-const keywordEl = document.getElementById('keyword');
+const formEl = document.getElementById("form");
 
 formEl.addEventListener('submit', function(e){
 	e.preventDefault();
-	verifyKeyword();
-	
-})
+	fetchSearch();
+});
 
-function verifyKeyword(){
-	const msgErreurSearchEl = document.getElementById('msgErreur');
-	if(keywordEl.value === NULL){
-		msgErreurSearchEl.innerHTML = "Champs ne peut pas être vide";
-	}else{
-		fetchValidSearch();
-	}
+
+function fetchSearch(){
+	const formEL = document.forms.namedItem("form");
+	var formData = new FormData(formEl);
+
+	fetch('validSearch.php', { method: 'POST, body: formData'})
+		.then(function(response) {
+			if (response.ok){
+				return response.json();
+			}else{
+				//TODO: ERREUR
+			}
+		})
+		.then(function(searchResult) {
+			// printResult(response.json());
+		})
 }
 
-function fetchValidSearch(){
-
-}
+// function printResult(searchResult){
+// 	const searchListEl = document.getElementById("searchList");
+// 	if(searchResult != ""){
+// 		searchList.innerHTML += "<li>maya00</li>"
+// 	}
+// }
